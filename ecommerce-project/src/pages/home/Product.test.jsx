40069abcb3cc7604d17b06f28360ccc330348580
Bharathi <rbharathi7995@gmail.com>
2026-06-08 20:13:41 +0530

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import axios from 'axios'
-import { it ,expect, describe, vi} from 'vitest'
+import { it ,expect, describe, vi, beforeEach} from 'vitest'
 import { render,screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { Product } from './Product'
 
 vi.mock('axios');
 
+
 describe('Product Component',() => {
+    let product; 
+    let loadCart ;
+
+    beforeEach(() => {
+      product={
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        rating: {
+          stars: 4.5,
+          count: 87
+        },
+        priceCents: 1090,
+        keywords: ["socks", "sports", "apparel"]
+      };
+
+      loadCart=vi.fn();
+        })
+
+
     it('displays the product details correctly ',() => {
-    const product= {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  }
-        const loadCart = vi.fn();
 
         render(<Product product={product} loadCart={loadCart} />);
   
@@ -46,18 +55,7 @@ describe('Product Component',() => {
     });
 
     it('add a product to the cart',async() => {
-        const product= {
-          id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-          image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-          name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-          rating: {
-            stars: 4.5,
-            count: 87
-          },
-          priceCents: 1090,
-          keywords: ["socks", "sports", "apparel"]
-        }
-        const loadCart = vi.fn();
+       
 
         render(<Product product={product} loadCart={loadCart} />);
    
